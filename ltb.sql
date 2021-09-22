e3c8dd253916c0286b4497aa8c582c0adba75524
sciahoo <marcinkporeda@gmail.com>
2021-09-22 17:12:37 +0200

    Union another cf3 to separate pcs from m2

--- ltb.sql
+++ ltb.sql
@@ -7,7 +7,7 @@ WITH cte AS
     PARENT_ID
     --CAST(0 AS varbinary(max)) AS Level
   FROM MATFOLDER
-  WHERE PARENT_ID = 19630
+  WHERE PARENT_ID = 19631 OR PARENT_ID = 19632 OR PARENT_ID = 19633 OR PARENT_ID = 19638
   UNION ALL
   SELECT
     i.DIR_ID,
@@ -28,6 +28,28 @@ cte2 AS
     TYPE,
     PARENT_ID
     --CAST(0 AS varbinary(max)) AS Level
+  FROM MATFOLDER
+  WHERE PARENT_ID = 20252 OR PARENT_ID = 20585 OR PARENT_ID = 20547
+  UNION ALL
+  SELECT
+    i.DIR_ID,
+    i.NAME,
+    i.TYPE,
+    i.PARENT_ID
+    --Level + CAST(i.DIR_ID AS varbinary(max)) AS Level
+  FROM MATFOLDER i
+  INNER JOIN cte2 c
+    ON c.DIR_ID = i.PARENT_ID
+)
+,
+cte3 AS
+(
+  SELECT
+    DIR_ID,
+    NAME,
+    TYPE,
+    PARENT_ID
+    --CAST(0 AS varbinary(max)) AS Level
   FROM PROFFOLDER
   WHERE PARENT_ID = 3575
   UNION ALL
@@ -38,9 +60,44 @@ cte2 AS
     i.PARENT_ID
     --Level + CAST(i.DIR_ID AS varbinary(max)) AS Level
   FROM PROFFOLDER i
-  INNER JOIN cte2 c
+  INNER JOIN cte3 c
     ON c.DIR_ID = i.PARENT_ID
 )
+
+SELECT
+M.BESTELLUNG AS Symbol,
+M.BESTELLUNG AS MatId,
+CASE
+	WHEN LEN(M.TEXT) - LEN(REPLACE(M.TEXT, 'x', '')) = 1
+		THEN
+SUBSTRING(M.TEXT,0,CHARINDEX('x',M.TEXT))
+ELSE ''
+END AS Length,
+CASE
+	WHEN LEN(M.TEXT) - LEN(REPLACE(M.TEXT, 'x', '')) = 1
+		THEN
+RIGHT(M.TEXT,CHARINDEX('x',REVERSE(M.TEXT))-1)
+ELSE ''
+END AS Width,
+M.THK AS Thicknes,
+0 AS MatType,
+M.COMMENT AS Description,
+M.MATCAT AS Groups,
+M.PRODUCER AS SubGroups,
+M.GRAIN AS Grain,
+'pcs' AS Unit,
+CASE
+	WHEN LEN(M.TEXT) - LEN(REPLACE(M.TEXT, 'x', '')) = 1
+		THEN 1 ELSE 0 END AS FlagActive,
+'logo.png' AS Picture,
+'logo.png' AS Picture1,
+'logo.png' AS Picture2
+FROM cte C
+LEFT JOIN MAT M ON C.NAME = M.NAME
+LEFT JOIN CMSRAWMAT R ON C.NAME = R.MATID
+WHERE TYPE = 100 AND M.BESTELLUNG <> ''
+
+UNION
 
 SELECT
 M.BESTELLUNG AS Symbol,
@@ -70,7 +127,7 @@ CASE
 'logo.png' AS Picture,
 'logo.png' AS Picture1,
 'logo.png' AS Picture2
-FROM cte C
+FROM cte2 C
 LEFT JOIN MAT M ON C.NAME = M.NAME
 LEFT JOIN CMSRAWMAT R ON C.NAME = R.MATID
 WHERE TYPE = 100 AND M.BESTELLUNG <> ''
@@ -95,6 +152,6 @@ CASE
 'logo.png' AS Picture,
 'logo.png' AS Picture1,
 'logo.png' AS Picture2
-FROM cte2 C
+FROM cte3 C
 LEFT JOIN PROFIL P ON C.NAME = P.NAME
 WHERE TYPE = 101 AND P.BESTELLUNG <> ''
